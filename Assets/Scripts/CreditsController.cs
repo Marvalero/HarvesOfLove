8360180c94e544bc764fd307aa0765f5dfee03c5
Maria Valero <maria.valero@thoughtworks.com>
2023-01-08 10:25:01 +0100

    Move the credits up the screen

--- Assets/Scripts/CreditsController.cs
+++ Assets/Scripts/CreditsController.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CreditsController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI creditsText;
+    public float speed = 50f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        var creditsRect = creditsText.rectTransform;
+        creditsRect.anchoredPosition += Vector2.up * speed * Time.deltaTime;
     }
 }
